472f15b34b5047f85bc0c611e09d1608fddbf50c
supritla <supritla@yahoo.com>
2014-03-04 16:14:42 -0500

    this is updated version

--- Git_Hub/src/gitHub_Testing/Eclipse_2_Git.java
+++ Git_Hub/src/gitHub_Testing/Eclipse_2_Git.java
@@ -5,7 +5,7 @@ public static void main (String [] args) {
 	
 	System.out.println("Hello this is version 1");
 	System.out.println("this is version 2!");
-	System.out.println("New Code");
+	
 }
 	
 	
